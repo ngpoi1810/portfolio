6c2557bb833363603516d63ab864170d8af7e90f
nguyen hai nguyen <nguyenhainguyen@bincg.com>
2022-09-19 17:01:43 +0700

    add taiwindcss portcss

---
+++ my-app/pages/index.js
@@ -0,0 +1,17 @@
+
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <>
+      <nav >
+        <div>
+          <Link href="#">
+            
+          </Link>
+        </div>
+      </nav>
+    </>
+    
+  )
+}
