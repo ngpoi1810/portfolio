d2458eb739934b1cfd55b69c96cb22d97ecff899
nguyen hai nguyen <nguyenhainguyen@bincg.com>
2022-09-20 17:03:27 +0700

    commit code

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -5,10 +5,14 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-    fontFamily: {
-      pacifico: ['Pacifico', cursive]
-    }
+    extend: {
+      fontFamily: {
+        'pacifico': ['Pacifico', 'cursive'],
+      },
+      backgroundImage: {
+        'hero-pattern':"url('/images/bg-hero.png')",
+      },
+    },
   },
   plugins: [],
 }
